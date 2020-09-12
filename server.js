7a67485795ef28474df8081b47a228d3c8e05ad1
9Sefa9 <sefagoevercin@hotmail.de>
2020-09-12 02:48:57 +0200

    Swiddle Projekt gestartet

---
+++ server.js
@@ -0,0 +1,18 @@
+	// call all the required packages
+    const express = require('express')
+    const bodyParser= require('body-parser')
+    const multer = require('multer');
+    const fs = require("fs");
+    const path = require("path");
+    //CREATE EXPRESS APP
+    const app = express();
+    const exec = require('child_process').exec
+
+//um auf CSS und Javascript zuzugreifen.
+app.use(express.static("scripts"));
+
+//zeigt den homepage
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.listen(8080, () => console.log('Server started on port 8080'));
