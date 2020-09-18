367f97d5997b5da2697344ac76318ff4897ce278
9Sefa9 <sefagoevercin@hotmail.de>
2020-09-18 03:20:23 +0200

    video hinzugefügt

--- server.js
+++ server.js
@@ -11,6 +11,10 @@
 //um auf CSS und Javascript zuzugreifen.
 app.use(express.static("scripts"));
 app.use('/images', express.static(__dirname + '/images'));
+app.use('/videos', express.static(__dirname + '/videos'));
+app.get('/favico.ico', (req, res) => {
+    res.sendStatus(404);
+});
 //zeigt den homepage
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
